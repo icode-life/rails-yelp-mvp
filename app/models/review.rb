7e774145d5bcecbdbb53fea769c4a96f67c9a33c
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2018-10-31 14:02:33 +0100

    routes seem okay

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  RATING = [1, 2, 3, 4, 5]
   validates :content, presence: true
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, presence: true, numericality: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+puts "[*] Starting"
+
+10.times do
+  name = Faker::Name.last_name
+  address = Faker::Address.full_address
+  category = %w(chinese italian japanese french belgian).sample
+  phone_number = Faker::PhoneNumber.phone_number
+  Restaurant.create!(name: name , category: category, address: address, phone_number: phone_number)
+end
+
+puts "[*] Done"
